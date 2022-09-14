482a542d7aa1cc5d0db3d8f3231765086e0464a0
rozran00@gmail.com <rozran00@gmail.com>
2022-09-14 19:36:29 -0400

    starting to build the rules

---
+++ README.md
@@ -0,0 +1,34 @@
+# Requirements
+
+- I want to prescribe what I should eat and do each day, based on some meta 
+requirements
+- Part 1 should be exercise
+- Part 2 should be food
+- Part 3 should be mind
+
+## For exercise:
+
+- Soccer is on Mondays and Thursdays (and scheduled)
+- I _should_ walk the dog on weekdays that I don't have class 
+- I need to lift at least 3 times a week
+- I need to run at least 2 times a week (soccer counts as running)
+- I need to have 1 [active] recovery day per week
+    - Long, light jog (i.e. 3 miles @ 10 min/mile)
+    - 30 minute low impact ride
+- I need to have 1 sauna session per week
+- I need to have at least one workout session per day
+    - Active Recovery
+    - Run
+    - Soccer
+    - Lift
+    - Walk longer than 1 hour
+    
+### Steps to get there:
+
+1. Work out what day I should do each thing
+    - If you've had 
+2. Work out how to add exclusions and recalculate (i.e. I have to travel or 
+something)
+3. Work out _when_ I should do each thing on each day (pull in schedule)
+4. Give warnings when I am behind 
+5. prioritize hard days after easy and vice-a-versa
--- scripts/whoop.py
+++ scripts/whoop.py
@@ -1,14 +1,22 @@
+# Libraries
+
 import requests
 import json
 import pandas as pd
 import sys
+from datetime import datetime, timedelta
 
-pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_columns', None)
 
+# Get UN and PW from file
 whoop_config = pd.read_csv("whoop.config")
 
+# API URL for Reference
+api_url = "https://api-7.whoop.com"
+
+# Get the Refresh Token
 refresh = requests.post(
-    url = "https://api-7.whoop.com/oauth/token",
+    url = f"{api_url}/oauth/token",
     headers = {
         "Content-Type": "application/json; charset=utf-8",
     },
@@ -22,8 +30,9 @@ refresh = requests.post(
 
 refresh_token = refresh.json()['refresh_token']
 
+# Get the access token
 response = requests.post(
-    url = "https://api-7.whoop.com/oauth/token",
+    url = f"{api_url}/oauth/token",
     headers = {
         "Content-Type": "application/json; charset=utf-8",
     },
@@ -33,19 +42,19 @@ response = requests.post(
     })
 )
 
+# Access token, user id, and start of this jawn
 access_token = response.json()['access_token']
+user_id = response.json()['user']['id']
+earliest = response.json()['user']['createdAt']
 
+latest = (datetime.today() + timedelta(days = 1)).strftime('%Y-%m-%dT00:00:00.000Z')
+
+# create a session
 s = requests.Session()
 s.headers.update({
     'Content-Type': 'application/json; charset=utf-8',
     'Authorization': f'Bearer {access_token}',
 })
-
-api_url = "https://api-7.whoop.com"
-user_id = "1508418"
-
-response = s.get(f'{api_url}/users/{user_id}')
-response.json()
 
 
 offset = 0
@@ -55,8 +64,8 @@ while offset < total_count:
     response = s.get(
         url=f'{api_url}/activities-service/v1/cycles/aggregate/range/{user_id}',
         params={
-            'startTime': '2021-08-11T00:00:00.000Z',
-            'endTime': '2022-09-15T00:00:00.000Z',
+            'startTime': earliest,
+            'endTime': latest,
             'limit': 50,
             'offset': offset
         })
@@ -64,16 +73,12 @@ while offset < total_count:
         break
 
     records.extend(response.json()['records'])
-    offset = response.json()['offset']
-    total_count = response.json()['total_count']
-    print(f'got {offset} of {total_count} items', end='\r')
     
 
 cycles = pd.DataFrame()
 sleeps = pd.DataFrame()
 recovery = pd.DataFrame()
 workouts = pd.DataFrame()
-v2_activities = pd.DataFrame()
 
 for record in range(len(records)):
     
@@ -96,12 +101,25 @@ for record in range(len(records)):
     tmp = pd.json_normalize(records[record]["workouts"])
     if tmp.shape[0] > 0:
         workouts = pd.concat([workouts, tmp], ignore_index = True)
-        
-    # v2_activities
-    tmp = pd.json_normalize(records[record]["v2_activities"])
-    if tmp.shape[0] > 0:
-        v2_activities = pd.concat([v2_activities, tmp], ignore_index = True)
     
 
 response = s.get(f'{api_url}/activities-service/v1/sports')
-sports = pd.DataFrame(response.json()).set_index('id')
+sports = pd.DataFrame(response.json())
+
+workouts = workouts.merge(sports[["id", "name"]], 
+                          left_on = "sport_id", 
+                          right_on = "id",
+                          how = "left")
+
+response = s.get(
+    url = f'{api_url}/users/{user_id}/metrics/heart_rate',
+    params = {
+        "start": "2022-09-06T15:00:00.000Z",
+        "end": "2022-09-14T15:00:00.000Z",
+        "step": "6", # every 6 seconds, 6 or 60 or 600
+    }
+)
+hr = pd.DataFrame.from_dict(response.json()['values'])
+hr['time'] = pd.to_datetime(hr['time'], unit = 'ms', utc = True)
+hr = hr.rename(columns={'data': 'bpm'})
+hr = hr.set_index('time')
